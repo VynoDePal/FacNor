9cfc682dc9529ddf8f1196b5623453e2f5c0bef8
VynoDePal <83235998+VynoDePal@users.noreply.github.com>
2026-06-10 03:18:44 +0100

    collegue: issue #52 — frontend/src/App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,7 @@
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+import React from "react";
+function App() {
+    return (_jsxs("div", { children: [_jsx("h1", { children: "FacNor - Gestion de Factures" }), _jsx("p", { children: "Bienvenue sur l'application de gestion de factures normalis\u00E9es." })] }));
+}
+export default App;
+//# sourceMappingURL=App.js.map
